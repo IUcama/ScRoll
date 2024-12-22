535379527753980f050c30b1d9a928cf9fe4310e
iucama <luca@camastro.de>
2024-12-22 23:11:26 +0100

    update server script, add debug

--- runScrollOnServer.sh
+++ runScrollOnServer.sh
@@ -1,17 +1,17 @@
 #!\bin\bash
 
-docker stop mongodb
-docker stop scroll
+sudo docker stop mongodb
+sudo docker stop scroll
 
-docker rm mongodb
-docker rm scroll
+sudo docker rm mongodb
+sudo docker rm scroll
 
-docker pull mongo:latest
-docker run -d --name mongodb mongo:latest
+sudo docker pull mongo:latest
+sudo docker run -d --name mongodb mongo:latest
 
-docker pull iucama/scroll:pr-1
-docker run -d --name scroll -p 3000:3000 -v /etc/letsencrypt/live/lucacamastro.de:/usr/src/app/certs --env-file .env.scroll iucama/scroll:pr-1
+sudo docker pull iucama/scroll:pr-1
+sudo docker run -d --name scroll -p 3000:3000 -v /etc/letsencrypt/live/lucacamastro.de:/usr/src/app/certs --env-file .env.scroll iucama/scroll:pr-1
 
 #docker network create scroll-network
-docker network connect scroll-network mongodb
-docker network connect scroll-network scroll
+sudo docker network connect scroll-network mongodb
+sudo docker network connect scroll-network scroll
