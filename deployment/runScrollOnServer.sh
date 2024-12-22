ee3d6761bf31a9727595fdc84ccea4a48492d794
iucama <luca@camastro.de>
2024-12-23 00:02:54 +0100

    add env-file to server (used in script)

--- deployment/runScrollOnServer.sh
+++ deployment/runScrollOnServer.sh
@@ -10,7 +10,7 @@ sudo docker pull mongo:latest
 sudo docker run -d --name mongodb mongo:latest
 
 sudo docker pull iucama/scroll:pr-1
-sudo docker run -d --name scroll -p 3000:3000 -v /etc/letsencrypt/live/lucacamastro.de:/usr/src/app/certs --env-file .env.scroll iucama/scroll:pr-1
+sudo docker run -d --name scroll -p 3000:3000 -v /etc/letsencrypt/live/lucacamastro.de:/usr/src/app/certs --env-file /home/luca/.env.scroll iucama/scroll:pr-1
 
 #docker network create scroll-network
 sudo docker network connect scroll-network mongodb
